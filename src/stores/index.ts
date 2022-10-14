db4f954f93c228a6aad7cd00b4563863d54b7001
Sangsoo Jin <swingwithblue@gmail.com>
2022-10-14 17:28:14 +0900

    Fix: Downgrade Vite to 3.1.4 (vitejs/vite #10430)

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,5 +1,6 @@
 import { atom } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
+import { focusAtom } from 'jotai/optics';
 
 export const googleMapsApiKeyAtom = atomWithStorage('googleMapsApiKey', '');
 
@@ -7,3 +8,11 @@ export const initialLocationAtom = atom<ICityLatLngPair>({
   city: '',
   latlng: { lat: 0, lng: 0 },
 });
+
+export const initialLocationLatAtom = focusAtom(initialLocationAtom, (optic) =>
+  optic.prop('latlng').prop('lat'),
+);
+
+export const initialLocationLngAtom = focusAtom(initialLocationAtom, (optic) =>
+  optic.prop('latlng').prop('lng'),
+);
