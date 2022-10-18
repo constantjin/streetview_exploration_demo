c13598e273f494add8410bb3230235f684d5a40f
Sangsoo Jin <swingwithblue@gmail.com>
2022-10-18 10:14:17 +0900

    Fix: Replace hard-coded basename in BrowserRouter
    Now use Vite-injected variable (import.meta.env.BASE_URL)
    Add comments about gh-pages plugin in vite.config.ts

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename="/streetview_exploration_demo">
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,18 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import jotaiDebugLabel from 'jotai/babel/plugin-debug-label';
 import jotaiReactRefresh from 'jotai/babel/plugin-react-refresh';
 
+// Plug-in to publish GitHub Pages on 'yarn build'
+// If you do not want to publish this application to the gh-page branch,
+// Just remove this import and 'ghPages' object in 'plugins' array
 import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Base name for this GitHub repository
+  // If you plan to fork this repo in your own repository,
+  // Please change this base name w.r.t your repository name
+  // E.g., repo name: 'streetview_exploration' -> base: '/streetview_exploration/'
+  // Reference: https://github.com/metonym/vite-plugin-gh-pages
   base: '/streetview_exploration_demo/',
   plugins: [
     react({ babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] } }),
